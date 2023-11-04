312607511066ad17962b58a13c8b9728e3c170d9
Aditya Gupta <rockonman3@gmail.com>
2023-11-04 15:51:38 +0530

    pdf renderer added with bitmap

---
+++ app/src/main/java/com/adityagupta/arcompanion/activities/LibraryActivity.kt
@@ -0,0 +1,36 @@
+package com.adityagupta.arcompanion.activities
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.adityagupta.arcompanion.activities.ui.main.SectionsPagerAdapter
+import com.adityagupta.arcompanion.databinding.ActivityLibraryBinding
+
+class LibraryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLibraryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = binding.fab
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
